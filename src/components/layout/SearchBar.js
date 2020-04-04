4274ab3f8ef945f322bff6d91318c06411785fed
chad Farrington <sandrock11@hotmail.com>
2020-04-04 01:20:43 -0400

    added log item components and preloader

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,26 @@
+/*
+
+        Search Component for the navbar
+*/
+
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <nav style={{ marginBottom: '30px' }} className='cyan darken-3'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input id='search' type='search' />
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons'>close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default SearchBar;
